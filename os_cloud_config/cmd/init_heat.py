f6b38565694fb49b68beb67a64f4c985ceb0dc5e
Steve Kowalik <steven@wedontsleep.org>
2014-03-31 18:55:13 +1100

    Add CLI scripts for init-{heat,keystone,swift}
    Add three CLI wrappers around the initialize methods in the keystone
    module for ease of use with other projects.
    
    Change-Id: I7b378660fd50e8595dd80ff2fa98e7f0324a5f8b

---
+++ os_cloud_config/cmd/init_heat.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from optparse import OptionParser
+import sys
+
+from os_cloud_config.keystone import initialize_for_heat
+
+
+def parse_args(args):
+    parser = OptionParser()
+    parser.add_option('-o', '--host', dest='host',
+                      help="ip/hostname of node where Keystone is running",
+                      metavar='HOST')
+    parser.add_option('-t', '--admin_token', dest='admin_token',
+                      help="admin token to use with Keystone's admin endpoint",
+                      metavar='ADMIN_TOKEN')
+    parser.add_option('-p', '--domain-admin-password',
+                      dest='domain_admin_password',
+                      help="heat domain admin's password to be set",
+                      metavar='DOMAIN_ADMIN_PASSWORD')
+    return parser.parse_args()
+
+
+def main():
+    (options, args) = parse_args(sys.argv)
+    if not options.host:
+        print("hostname of Keystone is required")
+        return 1
+    initialize_for_heat(options.host, options.admin_token,
+                        options.domain_admin_password)
