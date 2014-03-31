e7951984afd8b3b7a75c5659c847542acc840724
Steve Kowalik <steven@wedontsleep.org>
2014-03-31 17:04:12 +1100

    Add a register-nodes command and associated API
    As a first step of making register-nodes cleverer, port it from shell
    to Python, making use of the subprocess module to call nova or ironic.
    This slightly changes the interface, in that ironic usage is detected
    via Keystone, rather than use of an environment variable.
    
    Change-Id: I0d9ef1b3f19c55ed5b696871303a66808c3cf658

---
+++ os_cloud_config/nodes.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import subprocess
+
+
+def _get_id_line(lines, id_description, position=3):
+    for line in lines.split('\n'):
+        if id_description in line:
+            return line.split()[position]
+
+
+def register_nova_bm_node(service_host, node):
+    out = subprocess.check_output(["nova", "baremetal-node-create",
+                                  "--pm_address=%s" % node["pm_addr"],
+                                  "--pm_user=%s" % node["pm_user"],
+                                  "--pm_password=%s" % node["pm_password"],
+                                  service_host, node["cpu"], node["memory"],
+                                  node["disk"], node["mac"][0]])
+    bm_id = _get_id_line(out, " id ")
+    for mac in node["mac"][1:]:
+        subprocess.check_call(["nova", "baremetal-interface-add", bm_id, mac])
+
+
+def register_ironic_node(service_host, node):
+    out = subprocess.check_output(["ironic", "node-create", "-d",
+                                  node["pm_type"]])
+    node_id = _get_id_line(out, "uuid")
+    node_properties = ["properties/cpus=%s" % node["cpu"],
+                       "properties/memory_mb=%s" % node["memory"],
+                       "properties/local_gb=%s" % node["disk"],
+                       "properties/cpu_arch=%s" % node["arch"]]
+    if "impi" in node["pm_type"]:
+        pm_password = node["pm_password"]
+        impi_properties = ["driver_info/ipmi_address=%s" % node["pm_addr"],
+                           "driver_info/ipmi_username=%s" % node["pm_user"],
+                           "driver_info/impi_password=%s" % pm_password]
+        node_properties.extend(impi_properties)
+    elif node["pm_type"] == "pxe_ssh":
+        ssh = ("driver_info/ssh_key_filename=/mnt/state/var/lib/ironic/"
+               "virtual-power-key")
+        ssh_properties = ["driver_info/ssh_address=%s" % node["pm_addr"],
+                          "driver_info/ssh_username=%s" % node["pm_user"],
+                          ssh, "driver_info/ssh_virt_type=virsh"]
+        node_properties.extend(ssh_properties)
+    else:
+        raise Exception("Unknown pm_type: %s" % node["pm_type"])
+
+    subprocess.check_call(["ironic", "node-update", node_id, "add"]
+                          + node_properties)
+    for mac in node["mac"]:
+        subprocess.check_call(["ironic", "port-create", "-a", mac, "-n",
+                              node_id])
+
+
+def register_all_nodes(service_host, nodes_list):
+    if using_ironic():
+        register_func = register_ironic_node
+    else:
+        register_func = register_nova_bm_node
+    for node in nodes_list:
+        register_func(service_host, node)
+
+
+def check_nova_bm_service():
+    subprocess.check_call(["wait_for", "60", "10", "nova",
+                          "baremetal-node-create", "devtest_canary", "1", "1",
+                          "1", "11:22:33:44:55:66"])
+    out = subprocess.check_output(["nova", "baremetal-node-list"])
+    node_id = _get_id_line(out, "devtest_canary", position=1)
+    subprocess.check_call(["nova", "baremetal-node-delete", node_id])
+
+
+def check_ironic_service():
+    subprocess.check_call(["wait_for", "60", "10", "ironic", "chassis-create",
+                          "-d", "devtest_canary"])
+    out = subprocess.check_output(["ironic", "chassis-list"])
+    chassis_id = _get_id_line(out, "devtest_canary", position=1)
+    subprocess.check_call(["ironic", "chassis-delete", chassis_id])
+
+
+def check_service():
+    if using_ironic():
+        check_ironic_service()
+    else:
+        check_nova_bm_service()
+
+
+def using_ironic():
+    out = subprocess.check_output(["keystone", "service-list"])
+    for line in out.split('\n'):
+        if 'ironic' in line:
+            return True
+    return False
