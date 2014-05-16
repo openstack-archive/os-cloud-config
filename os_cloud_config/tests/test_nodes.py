74330343e75778b04d86690fe86f09183209ee50
Steve Kowalik <steven@wedontsleep.org>
2014-05-16 13:30:06 -0400

    Switch from subprocess to ironicclient
    As one large improvement to register-nodes, stop using subprocess to
    fork off the ironic CLI, and instead use python-ironicclient and its
    Python API to register nodes. This changes behaviour to spin for 600
    seconds until a node is registered.
    
    Change-Id: I2fd51873b892026c21045e6b912ef35b0be66fc5

--- os_cloud_config/tests/test_nodes.py
+++ os_cloud_config/tests/test_nodes.py
@@ -13,8 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import collections
+
 import mock
 
+from ironicclient.openstack.common.apiclient import exceptions as ironicexp
 from os_cloud_config import nodes
 from os_cloud_config.tests import base
 
@@ -41,38 +44,58 @@ class NodesTest(base.TestCase):
              "2048", "30", "aaa"])
         check_mock.has_calls([nova_bm_call, nova_bm_call])
 
-    @mock.patch('os_cloud_config.nodes._check_output')
+    @mock.patch('os.environ')
+    @mock.patch('ironicclient.client.get_client')
+    def test_get_ironic_client(self, client_mock, environ):
+        nodes._get_ironic_client()
+        client_mock.assert_called_once_with(
+            1, os_username=environ["OS_USERNAME"],
+            os_password=environ["OS_PASSWORD"],
+            os_auth_url=environ["OS_AUTH_URL"],
+            os_tenant_name=environ["OS_TENANT_NAME"])
+
     @mock.patch('os_cloud_config.nodes.using_ironic', return_value=True)
-    @mock.patch('os_cloud_config.nodes._get_id_line', return_value="1")
-    @mock.patch('subprocess.check_call')
-    def test_register_all_nodes_ironic(self, check_call, id_mock, ironic_mock,
-                                       check_output_mock):
+    def test_register_all_nodes_ironic(self, using_ironic):
         node_list = [self._get_node(), self._get_node()]
         node_list[1]["pm_type"] = "ipmi"
-        nodes.register_all_nodes('servicehost', node_list)
-        pxe_node_create = mock.call(
-            ["ironic", "node-create", "-d", "pxe_ssh"])
-        ipmi_node_create = mock.call(
-            ["ironic", "node-create", "-d", "ipmi"])
-        check_output_mock.has_calls([pxe_node_create, ipmi_node_create])
+        node_properties = {"cpus": "1",
+                           "memory_mb": "2048",
+                           "local_gb": "30",
+                           "cpu_arch": "amd64"}
+        ironic = mock.MagicMock()
+        nodes.register_all_nodes('servicehost', node_list, client=ironic)
         ssh_key = "/mnt/state/var/lib/ironic/virtual-power-key"
-        common_update_args = [
-            "ironic", "node-update", "1", "add", "properties/cpus=1",
-            "properties/memory_mb=2048", "properties/local_gb=30",
-            "properties/cpu_arch=amd64"]
-        pxe_node_update_call = mock.call(
-            common_update_args + ["driver_info/ssh_address=foo.bar",
-                                  "driver_info/ssh_username=test",
-                                  "driver_info/ssh_key_filename=%s" % ssh_key,
-                                  "driver_info/ssh_virt_type=virsh"])
-        ipmi_node_update_call = mock.call(
-            common_update_args + ["driver_info/ipmi_address=foo.bar",
-                                  "driver_info/ipmi_username=test",
-                                  "driver_info/ipmi_password=random"])
-        power_off_call = mock.call(
-            ["ironic", "node-set-power-state", "1", "off"])
-        port_update_call = mock.call(
-            ["ironic", "port-create", "-a", "aaa", "-n", "1"])
-        check_call.assert_has_calls(
-            [pxe_node_update_call, power_off_call, port_update_call,
-             ipmi_node_update_call, power_off_call, port_update_call])
+        pxe_node_driver_info = {"ssh_address": "foo.bar",
+                                "ssh_username": "test",
+                                "ssh_key_filename": ssh_key,
+                                "ssh_virt_type": "virsh"}
+        ipmi_node_driver_info = {"ipmi_address": "foo.bar",
+                                 "ipmi_username": "test",
+                                 "ipmi_password": "random"}
+        pxe_node = mock.call(driver="pxe_ssh",
+                             driver_info=pxe_node_driver_info,
+                             properties=node_properties)
+        port_call = mock.call(node_uuid=ironic.node.create.return_value.uuid,
+                              address='aaa')
+        power_off_call = mock.call(ironic.node.create.return_value.uuid, 'off')
+        ipmi_node = mock.call(driver="ipmi",
+                              driver_info=ipmi_node_driver_info,
+                              properties=node_properties)
+        ironic.node.create.assert_has_calls([pxe_node, ipmi_node])
+        ironic.port.create.assert_has_calls([port_call, port_call])
+        ironic.node.set_power_state.assert_has_calls(
+            [power_off_call, power_off_call])
+
+    @mock.patch('time.sleep')
+    def test_register_ironic_node_retry(self, sleep):
+        ironic = mock.MagicMock()
+        ironic_node = collections.namedtuple('node', ['uuid'])
+        side_effect = (ironicexp.ConnectionRefused,
+                       ironicexp.ServiceUnavailable, ironic_node('1'))
+        ironic.node.create.side_effect = side_effect
+        nodes.register_ironic_node(None, self._get_node(), client=ironic)
+        sleep.assert_has_calls([mock.call(10), mock.call(10)])
+        node_create = mock.call(driver='pxe_ssh',
+                                driver_info=mock.ANY,
+                                properties=mock.ANY)
+        ironic.node.create.assert_has_calls(node_create)
