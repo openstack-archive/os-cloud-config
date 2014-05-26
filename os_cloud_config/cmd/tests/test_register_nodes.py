1c6fa34d10ce8bf2028ca1a1514d26583a392782
Steve Kowalik <steven@wedontsleep.org>
2014-05-26 11:23:05 +1000

    Check for relevant environment variables
    Currently, register-nodes does not check for environment variables on
    starting, which leads to a messy traceback from subprocess, as well
    as an error from keystone list. Check and exit before we check for
    services.
    
    Change-Id: Id838fe47ff117182eddc69344e018b67aee44419

--- os_cloud_config/cmd/tests/test_register_nodes.py
+++ os_cloud_config/cmd/tests/test_register_nodes.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import sys
 import tempfile
 
@@ -26,6 +27,8 @@ class RegisterNodesTest(base.TestCase):
 
     @mock.patch('os_cloud_config.nodes.register_all_nodes')
     @mock.patch('os_cloud_config.nodes.check_service')
+    @mock.patch.object(os, 'environ', {'OS_USERNAME': 'a', 'OS_PASSWORD': 'a',
+                       'OS_TENANT_NAME': 'a', 'OS_AUTH_URL': 'a'})
     @mock.patch.object(sys, 'argv', ['register-nodes', '--service-host',
                        'seed', '--nodes'])
     def test_with_arguments(self, register_mock, check_mock):
@@ -36,3 +39,14 @@ class RegisterNodesTest(base.TestCase):
             register_nodes.main()
         self.assertEqual(1, check_mock.call_count)
         register_mock.has_calls([mock.call("seed", "{}")])
+
+    @mock.patch('os_cloud_config.nodes.check_service')
+    @mock.patch.object(os, 'environ', {'OS_USERNAME': 'b'})
+    @mock.patch.object(sys, 'argv', ['register-nodes', '--nodes'])
+    def test_with_no_environment(self, check_mock):
+        with tempfile.NamedTemporaryFile() as f:
+            f.write('{}\n')
+            f.flush()
+            sys.argv.append(f.name)
+            register_nodes.main()
+        self.assertEqual(0, check_mock.call_count)
