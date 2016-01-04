8cd06b0482ce4b247d326e9025c50b2f391c6432
Dmitry Tantsur <dtantsur@redhat.com>
2016-01-04 16:34:34 +0100

    [WIP] Rewrite node creating and updating code
    This change started as an attempt to stop hardcoding ironic properties,
    but turns out a lot of things need fixing. Highlights:
    * Stop hardcoding flavor (e.g. agent, pxe) for each driver
    * Support only generic properties with pm_* names. Driver-specific things
      should stay with their prefix. Existing pm_* driver-specific things
      were deprecated.
    * Pass through everything that starts with driver-specific prefix to
      node's driver_info.
    * Dropped handling Conflict exceptions - ironicclient is doing it for
      some time already (and does better job in it).
    * Issue a specific exception for malformed instackenv.json.
    * Use 'add' operation instead of 'replace', as it allows both adding
      and overwriting (despite its name).
    * Fixed some small issues like adding a dict to a set.
    
    Change-Id: Id3fb1a20e9790ce7e45f1fac97ddea4f8edc8ac3

--- os_cloud_config/tests/test_nodes.py
+++ os_cloud_config/tests/test_nodes.py
@@ -15,7 +15,6 @@
 
 import collections
 
-from ironicclient import exc as ironicexp
 import mock
 from testtools import matchers
 
@@ -30,201 +29,6 @@ class NodesTest(base.TestCase):
                 'mac': ['aaa'], 'pm_addr': 'foo.bar', 'pm_user': 'test',
                 'pm_password': 'random', 'pm_type': 'pxe_ssh', 'name': 'node1',
                 'capabilities': 'num_nics:6'}
-
-    def test_register_list_of_nodes(self):
-        nodes_list = ['aaa', 'bbb']
-        return_node = nodes_list[0]
-        register_func = mock.MagicMock()
-        register_func.side_effect = [return_node, ironicexp.Conflict]
-        seen = nodes._register_list_of_nodes(register_func, {}, None,
-                                             nodes_list, False, 'servicehost',
-                                             None, None)
-        self.assertEqual(seen, set(nodes_list))
-
-    def test_extract_driver_info_ipmi(self):
-        node = self._get_node()
-        node["pm_type"] = "ipmi"
-        expected = {"ipmi_address": "foo.bar",
-                    "ipmi_username": "test",
-                    "ipmi_password": "random"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_ipmi_extended(self):
-        node = self._get_node()
-        node["pm_type"] = "ipmi"
-        node["ipmi_bridging"] = "dual"
-        node["ipmi_transit_address"] = "0x42"
-        node["ipmi_transit_channel"] = "0"
-        node["ipmi_target_address"] = "0x41"
-        node["ipmi_target_channel"] = "1"
-        node["ipmi_local_address"] = "0"
-        expected = {"ipmi_address": "foo.bar",
-                    "ipmi_username": "test",
-                    "ipmi_password": "random",
-                    "ipmi_bridging": "dual",
-                    "ipmi_transit_address": "0x42",
-                    "ipmi_transit_channel": "0",
-                    "ipmi_target_address": "0x41",
-                    "ipmi_target_channel": "1",
-                    "ipmi_local_address": "0",
-                    }
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_ssh(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_ssh"
-        expected = {"ssh_address": "foo.bar",
-                    "ssh_username": "test",
-                    "ssh_key_contents": "random",
-                    "ssh_virt_type": "virsh"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_drac(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_drac"
-        expected = {"drac_host": "foo.bar",
-                    "drac_username": "test",
-                    "drac_password": "random"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_ssh_with_pm_virt_type(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_ssh"
-        node["pm_virt_type"] = "vbox"
-        expected = {"ssh_address": "foo.bar",
-                    "ssh_username": "test",
-                    "ssh_key_contents": "random",
-                    "ssh_virt_type": "vbox"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_iboot(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_iboot"
-        expected = {"iboot_address": "foo.bar",
-                    "iboot_username": "test",
-                    "iboot_password": "random"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_iboot_with_pm_relay_id(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_iboot"
-        node["pm_relay_id"] = "pxe_iboot_id"
-        expected = {"iboot_address": "foo.bar",
-                    "iboot_username": "test",
-                    "iboot_password": "random",
-                    "iboot_relay_id": "pxe_iboot_id"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_iboot_with_pm_port(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_iboot"
-        node["pm_port"] = "8080"
-        expected = {"iboot_address": "foo.bar",
-                    "iboot_username": "test",
-                    "iboot_password": "random",
-                    "iboot_port": "8080"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_ucs(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_ucs"
-        node["pm_service_profile"] = "foo_profile"
-        expected = {"ucs_hostname": "foo.bar",
-                    "ucs_username": "test",
-                    "ucs_password": "random",
-                    "ucs_service_profile": "foo_profile"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_irmc(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_irmc"
-        expected = {"irmc_address": "foo.bar",
-                    "irmc_username": "test",
-                    "irmc_password": "random"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_irmc_with_irmc_port(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_irmc"
-        node["pm_port"] = "443"
-        expected = {"irmc_address": "foo.bar",
-                    "irmc_username": "test",
-                    "irmc_password": "random",
-                    "irmc_port": "443"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_irmc_with_irmc_auth_method(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_irmc"
-        node["pm_auth_method"] = "baz_auth_method"
-        expected = {"irmc_address": "foo.bar",
-                    "irmc_username": "test",
-                    "irmc_password": "random",
-                    "irmc_auth_method": "baz_auth_method"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_irmc_with_irmc_client_timeout(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_irmc"
-        node["pm_client_timeout"] = "60"
-        expected = {"irmc_address": "foo.bar",
-                    "irmc_username": "test",
-                    "irmc_password": "random",
-                    "irmc_client_timeout": "60"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_irmc_with_irmc_sensor_method(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_irmc"
-        node["pm_sensor_method"] = "ipmitool"
-        expected = {"irmc_address": "foo.bar",
-                    "irmc_username": "test",
-                    "irmc_password": "random",
-                    "irmc_sensor_method": "ipmitool"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_iscsi_irmc(self):
-        node = self._get_node()
-        node["pm_type"] = "iscsi_irmc"
-        node["pm_deploy_iso"] = "deploy.iso"
-        expected = {"irmc_address": "foo.bar",
-                    "irmc_username": "test",
-                    "irmc_password": "random",
-                    "irmc_deploy_iso": "deploy.iso"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_agent_irmc(self):
-        node = self._get_node()
-        node["pm_type"] = "agent_irmc"
-        node["pm_deploy_iso"] = "deploy.iso"
-        expected = {"irmc_address": "foo.bar",
-                    "irmc_username": "test",
-                    "irmc_password": "random",
-                    "irmc_deploy_iso": "deploy.iso"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_ipmi_with_kernel_ramdisk(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_ipmi"
-        node["kernel_id"] = "kernel-abc"
-        node["ramdisk_id"] = "ramdisk-foo"
-        expected = {"ipmi_address": "foo.bar",
-                    "ipmi_username": "test",
-                    "ipmi_password": "random",
-                    "deploy_kernel": "kernel-abc",
-                    "deploy_ramdisk": "ramdisk-foo"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_pxe_wol(self):
-        node = self._get_node()
-        node["pm_type"] = "pxe_wol"
-        expected = {"wol_host": "foo.bar"}
-        self.assertEqual(expected, nodes._extract_driver_info(node))
-
-    def test_extract_driver_info_unknown_type(self):
-        node = self._get_node()
-        node["pm_type"] = "unknown_type"
-        self.assertRaises(ValueError, nodes._extract_driver_info, node)
 
     def test_register_all_nodes_ironic_no_hw_stats(self):
         node_list = [self._get_node()]
@@ -256,7 +60,7 @@ class NodesTest(base.TestCase):
         ironic.port.create.assert_has_calls([port_call])
         ironic.node.set_power_state.assert_has_calls([power_off_call])
 
-    def test_register_all_nodes_ironic(self):
+    def test_register_all_nodes(self):
         node_list = [self._get_node()]
         node_properties = {"cpus": "1",
                            "memory_mb": "2048",
@@ -280,7 +84,7 @@ class NodesTest(base.TestCase):
         ironic.port.create.assert_has_calls([port_call])
         ironic.node.set_power_state.assert_has_calls([power_off_call])
 
-    def test_register_all_nodes_ironic_kernel_ramdisk(self):
+    def test_register_all_nodes_kernel_ramdisk(self):
         node_list = [self._get_node()]
         node_properties = {"cpus": "1",
                            "memory_mb": "2048",
@@ -312,31 +116,7 @@ class NodesTest(base.TestCase):
         ironic.port.create.assert_has_calls([port_call])
         ironic.node.set_power_state.assert_has_calls([power_off_call])
 
-    @mock.patch('time.sleep')
-    def test_register_ironic_node_retry(self, sleep):
-        ironic = mock.MagicMock()
-        ironic_node = collections.namedtuple('node', ['uuid'])
-        side_effect = (ironicexp.ConnectionRefused,
-                       ironicexp.ServiceUnavailable, ironic_node('1'))
-        ironic.node.create.side_effect = side_effect
-        nodes.register_ironic_node(None, self._get_node(), client=ironic)
-        sleep.assert_has_calls([mock.call(10), mock.call(10)])
-        node_create = mock.call(driver='pxe_ssh',
-                                name='node1',
-                                driver_info=mock.ANY,
-                                properties=mock.ANY)
-        ironic.node.create.assert_has_calls([node_create, node_create,
-                                             node_create])
-
-    @mock.patch('time.sleep')
-    def test_register_ironic_node_failure(self, sleep):
-        ironic = mock.MagicMock()
-        ironic.node.create.side_effect = ironicexp.ConnectionRefused
-        self.assertRaises(ironicexp.ServiceUnavailable,
-                          nodes.register_ironic_node, None, self._get_node(),
-                          client=ironic)
-
-    def test_register_ironic_node_update(self):
+    def test_register_update(self):
         node = self._get_node()
         ironic = mock.MagicMock()
         node_map = {'mac': {'aaa': 1}}
@@ -351,12 +131,15 @@ class NodesTest(base.TestCase):
                 {'path': '/properties/cpu_arch', 'value': 'amd64'},
                 {'path': '/properties/cpus', 'value': '1'},
                 {'path': '/properties/capabilities', 'value': 'num_nics:6'},
-                {'path': '/driver_info/ssh_username', 'value': 'test'}]
+                {'path': '/driver_info/ssh_username', 'value': 'test'},
+                {'path': '/driver_info/ssh_virt_type', 'value': 'virsh'}]
             for key in update_patch:
-                key['op'] = 'replace'
+                key['op'] = 'add'
             self.assertThat(update_patch,
                             matchers.MatchesSetwise(*(map(matchers.Equals,
                                                           args[1]))))
+            return mock.Mock(uuid='uuid1')
+
         ironic.node.update.side_effect = side_effect
         nodes._update_or_register_ironic_node(None, node, node_map,
                                               client=ironic)
@@ -386,7 +169,7 @@ class NodesTest(base.TestCase):
     def test_update_node_ironic_pxe_irmc(self):
         self._update_by_type('pxe_irmc')
 
-    def test_register_ironic_node_update_uppercase_mac(self):
+    def test_register_node_update_uppercase_mac(self):
         node = self._get_node()
         node['mac'][0] = node['mac'][0].upper()
         ironic = mock.MagicMock()
@@ -402,28 +185,20 @@ class NodesTest(base.TestCase):
                 {'path': '/properties/cpu_arch', 'value': 'amd64'},
                 {'path': '/properties/cpus', 'value': '1'},
                 {'path': '/properties/capabilities', 'value': 'num_nics:6'},
-                {'path': '/driver_info/ssh_username', 'value': 'test'}]
+                {'path': '/driver_info/ssh_username', 'value': 'test'},
+                {'path': '/driver_info/ssh_virt_type', 'value': 'virsh'}]
             for key in update_patch:
-                key['op'] = 'replace'
+                key['op'] = 'add'
             self.assertThat(update_patch,
                             matchers.MatchesSetwise(*(map(matchers.Equals,
                                                           args[1]))))
+            return mock.Mock(uuid='uuid1')
 
         ironic.node.update.side_effect = side_effect
         nodes._update_or_register_ironic_node(None, node, node_map,
                                               client=ironic)
         ironic.node.update.assert_called_once_with(
             ironic.node.get.return_value.uuid, mock.ANY)
-
-    @mock.patch('time.sleep')
-    def test_register_ironic_node_update_locked_node(self, sleep):
-        node = self._get_node()
-        ironic = mock.MagicMock()
-        ironic.node.update.side_effect = ironicexp.Conflict
-        node_map = {'mac': {'aaa': 1}}
-        self.assertRaises(ironicexp.Conflict,
-                          nodes._update_or_register_ironic_node, None, node,
-                          node_map, client=ironic)
 
     def test_register_ironic_node_int_values(self):
         node_properties = {"cpus": "1",
@@ -477,12 +252,15 @@ class NodesTest(base.TestCase):
                 {'path': '/properties/cpu_arch', 'value': 'amd64'},
                 {'path': '/properties/cpus', 'value': '1'},
                 {'path': '/properties/capabilities', 'value': 'num_nics:6'},
-                {'path': '/driver_info/ssh_username', 'value': 'test'}]
+                {'path': '/driver_info/ssh_username', 'value': 'test'},
+                {'path': '/driver_info/ssh_virt_type', 'value': 'virsh'}]
             for key in update_patch:
-                key['op'] = 'replace'
+                key['op'] = 'add'
             self.assertThat(update_patch,
                             matchers.MatchesSetwise(*(map(matchers.Equals,
                                                       args[1]))))
+            return mock.Mock(uuid='uuid1')
+
         ironic.node.update.side_effect = side_effect
         nodes._update_or_register_ironic_node(None, node, node_map,
                                               client=ironic)
