85a624ca4281e71b322419873cfbeda6e94ffe79
Ryota MIBU <r-mibu@cq.jp.nec.com>
2016-01-25 23:20:18 +0900

    add aodh and gnocchi to keystone service list
    This patch adds default values for aodh and gnocchi service/endpoint
    to the keystone service list in order to make those services available
    in TripleO setup phase.
    
    Note: the list is to have default values and not all services will be
    created automatically.
    
    Note: alarming function in ceilometer is deprecated by aodh in Liberty
    and will be removed in Mitaka. So, we need aodh to have equivalent
    functionality of existing ceilometer.
    
    Change-Id: Ib6c9c4c35da3fb55e0ca8e2d5a58ebaf4204d792

--- os_cloud_config/keystone.py
+++ os_cloud_config/keystone.py
@@ -75,6 +75,19 @@ SERVICES = {
         'description': 'Ceilometer Service',
         'type': 'metering',
         'port': 8777,
+        'ssl_port': 13777,
+    },
+    'gnocchi': {
+        'description': 'OpenStack Metric Service',
+        'type': 'metric',
+        'port': 8041,
+        'ssl_port': 13041,
+    },
+    'aodh': {
+        'description': 'OpenStack Alarming Service',
+        'type': 'alarming',
+        'port': 8042,
+        'ssl_port': 13042,
     },
     'cinder': {
         'description': 'Cinder Volume Service',
